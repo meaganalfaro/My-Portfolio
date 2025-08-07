f8949b6e25ec52a0d1ea669ec06d1ccb5e3ae69d
Meagan Alfaro <meaganalfaro1@gmail.com>
2025-08-07 16:55:47 -0400

    About me section done, with working links and nav bar correctly points to the About section

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,30 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import backgroundImage from './assets/new-background.png'
 import './App.css'
 import './styles/design-system.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import Home from './pages/Home'
+import AboutMe from './pages/AboutMe'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
-      <Hero />
-      <style>
-        {`
-          body {
-            background-image: url(${backgroundImage});
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-          }
-        `}
-      </style>
+      <Home />
+      <AboutMe />
     </>
   )
 }
